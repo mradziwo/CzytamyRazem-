1edd7be615b03e21f9da7aee1643e8c9edd042d0
Mike <mradziwo@users.noreply.github.com>
2025-04-30 20:37:38 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CzytamRazemApp from './components/CzytamRazemApp';
+
+function App() {
+  return (
+    <div>
+      <h1>Czytam Razem</h1>
+      <CzytamRazemApp />
+    </div>
+  );
+}
+
+export default App;
